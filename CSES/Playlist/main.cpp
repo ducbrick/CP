921bd508d376d9e4989a153c364c9656595e2000
ducbrick <ducbrick@gmail.com>
2025-07-15 01:06:18 +0700

    update

---
+++ CSES/Playlist/main.cpp
@@ -0,0 +1,45 @@
+// Problem: Playlist
+// Contest: CSES Problem Set
+// Judge: CSES
+// URL: https://cses.fi/problemset/task/1141
+// Memory Limit: 512
+// Time Limit: 1000
+// By DUCBRICK
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 2e5;
+
+int a[MAX_N + 1];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	set <int> f;
+
+	int res = 0;
+
+	for (int l = 1, r = 1; r <= n; r++) {
+		while (f.count(a[r])) {
+			f.erase(a[l]);
+			l++;
+		}
+
+		f.insert(a[r]);
+		res = max(res, r - l + 1);
+	}
+
+	cout << res;
+
+	return 0;
+}
